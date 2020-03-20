d3d5b3fbaefa05ead5bb45e872d196e4b65e0e46
Konrad Zolynski <konrad.zolynski@gmail.com>
2020-03-20 15:56:52 +0100

    Addition of initial project with Main class and proper .gitignore file

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * @author Konrad ¯o³yñski | https://github.com/Konrad-code | konrad.zolynski@gmail.com | +48 533 683 168
+ *
+ * @author Janek Misiórski | https://github.com/janekjmf 	| janekjmf@gmail.com 		| +48 883 483 807
+ */
+public class Main {
+	
+}
