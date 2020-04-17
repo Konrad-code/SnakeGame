027202ebea294f6abbd0293b18fda150147ac543
janekjmf <janekjmf@gmail.com>
2020-04-17 03:00:18 +0200

    Poprawki

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ import java.util.Random;
  *
  * @author Janek Misiórski | https://github.com/janekjmf 	| janekjmf@gmail.com 		| +48 883 483 807
  */
+
 public class Main
 {
     public static void main(String[] args)
@@ -19,7 +20,7 @@ public class Main
 
         ramka.add(okno);
         ramka.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        ramka.setTitle("Wonsz Rzeczny");
+        ramka.setTitle("Zaskroniec");
         ramka.setResizable(false);
         ramka.pack();
         ramka.setLocationRelativeTo(null);
@@ -30,7 +31,6 @@ public class Main
 class Okno extends JPanel implements Runnable, KeyListener
 {
     public static final int szerokosc = 400, wysokosc = 400;
-    private int szybkosc = 7;//im wiêcej tym wolniej
     private Thread thread;
     private boolean dalejPelza = false;
     private Wonsz wonsz;
@@ -38,16 +38,19 @@ class Okno extends JPanel implements Runnable, KeyListener
     private Jablko jablko;
     private ArrayList<Jablko> jablka;
     private Random random;
-    private int x = 10, y = 10;
-    private int size = 5;
-
+    private int X = 10;
+    private int Y = 10;
+    public int size = 5;
+    final private int startSize = size;
+    public boolean info = false;
+    boolean pauza = false;
+    private boolean przechodzeniePrzezScany = true; // true - mo¿na przechodziæ przez œciany
     private boolean prawo = true;
     private boolean lewo = false;
     private boolean gora = false;
     private boolean dol =false;
-
-    private int z = 0;
-
+    private int czekanie = 70;
+    private int stopienSzybkosci = 5;
 
     public Okno()
     {
@@ -62,22 +65,80 @@ class Okno extends JPanel implements Runnable, KeyListener
 
     public void paint(Graphics g)
     {
-        g.clearRect(0, 0, szerokosc, wysokosc);
+        g.clearRect(0, 0, szerokosc, szerokosc);
         g.setColor(Color.WHITE);
-        g.fillRect(0, 0, szerokosc, wysokosc);
-
-
-        for (int i = 0; i < szerokosc / 10; i++)
-            g.drawLine(i * 10, 0, i * 10, wysokosc);
-
-        for (int i = 0; i < wysokosc / 10; i++)
-            g.drawLine(0, i * 10, szerokosc, i * 10);
+        g.fillRect(0, 0, szerokosc, szerokosc);
 
         for (int i = 0; i < snake.size(); i++)
-            snake.get(i).draw(g);
+        {
+            if (i == snake.size()-1)
+                snake.get(i).draw(g, 1600);
+            else
+                snake.get(i).draw(g, i);
+        }
 
-        for(int i = 0; i < jablka.size(); i++)
+        for (int i = 0; i < jablka.size(); i++)
             jablka.get(i).draw(g);
+
+        Font font = new Font("Arial", Font.BOLD, 14);
+        g.setFont(font);
+        g.setColor(Color.GRAY);
+        g.drawOval(5,5,10,10);
+        g.drawString(": "+(snake.size() - startSize), 20, 15);
+        Font font2 = new Font("Arial Black", Font.ITALIC, 10);
+        Font font3 = new Font("Arial Black", Font.ITALIC, 40);
+        g.setFont(font2);
+
+        if (info)
+        {
+            g.setColor(Color.RED);
+            g.fillRect(7, 30, 70, 15);//X Wê¿a
+            g.fillRect(7, 50, 70, 15);//Y Wê¿a
+            g.fillRect(7, 70, 110, 15);//D³ugoœæ Wê¿a
+            g.fillRect(7, 90, 110, 15);//Prêdkoœæ Wê¿a
+            g.fillRect(7, 110, 80, 15);//X jab³ka
+            g.fillRect(7, 130, 80, 15);//Y jab³ka
+            g.fillRect(7, 170, 110, 15);//Informacje
+            g.fillRect(7, 190, 240, 15);//Przechodzenie przez œciany
+            g.fillRect(7, 210, 95, 15);//Pauza
+            g.fillRect(7, 230, 190, 15);//Zmniejsz prêdkoœæ
+            g.fillRect(7, 250, 185, 15);//Zwiêksz prêdkoœæ
+
+            g.setColor(Color.WHITE);
+            g.drawString("X Wê¿a: " + X, 10, 40);
+            g.drawString("Y Wê¿a: " + Y, 10, 60);
+            g.drawString("D³ugoœæ Wê¿a: " + (snake.size()), 10, 80);
+            g.drawString("Prêdkoœæ Wê¿a: " + (stopienSzybkosci), 10, 100);
+            g.drawString("X jab³ka : " + jablko.getX(), 10, 120);
+            g.drawString("Y jab³ka : " + jablko.getY(), 10, 140);
+            g.drawString("\" I \" -  Informacje", 10, 180);
+            g.drawString("\" O \" -  Przechodzenie przez œciany: " + przechodzeniePrzezScany, 10, 200);
+            g.drawString("\" Esc \" -  Pauza", 10, 220);
+            g.drawString("\" Z \" -  Zmniejsz prêdkoœæ wê¿a", 10, 240);
+            g.drawString("\" X \" -  Zwiêksz prêdkoœæ wê¿a", 10, 260);
+        }
+
+        if (pauza)
+        {
+            g.setFont(font3);
+            g.setColor(Color.RED);
+            g.fillRect(137, 153, 140, 50);
+            g.setColor(Color.WHITE);
+            g.drawString("Pauza", 140, 190);
+            g.setColor(Color.GRAY);
+            g.setFont(font2);
+        }
+
+        if (!dalejPelza)
+        {
+            g.setFont(font3);
+            g.setColor(Color.RED);
+            g.fillRect(100, 253, 235, 50);
+            g.setColor(Color.WHITE);
+            g.drawString("Przegra³eœ", 100, 290);
+            g.setColor(Color.GRAY);
+            g.setFont(font2);
+        }
     }
 
     public void start()
@@ -97,68 +158,77 @@ class Okno extends JPanel implements Runnable, KeyListener
     {
         while (dalejPelza)
         {
-            if (snake.size() == 0)
+            if(!pauza)
             {
-                wonsz = new Wonsz(x, y, 10);
-                snake.add(wonsz);
-            }
-            if(jablka.size() == 0)
-            {
-                int xCoor = random.nextInt(39);
-                int yCoor = random.nextInt(39);
-
-                jablko = new Jablko(xCoor, yCoor, 10);
-                jablka.add(jablko);
-            }
-
-            for(int i = 0; i < jablka.size(); i++)
-            {
-                if(x == jablka.get(i).getX() && y == jablka.get(i).getY())
+                if (snake.size() == 0)
                 {
-                    size++;
-                    jablka.remove(i);
-                    i++;
+                    wonsz = new Wonsz(X, Y, 10);
+                    snake.add(wonsz);
                 }
-            }
-
-            for(int i =0; i < snake.size(); i++)
-            {
-                if(x == snake.get(i).getX() && y == snake.get(i).getY())
+                if(jablka.size() == 0)
                 {
-                    if(i != snake.size() - 1)
+                    int xJablka = random.nextInt(39);
+                    int yJablka = random.nextInt(39);
+
+                    jablko = new Jablko(xJablka, yJablka, 10);
+                    jablka.add(jablko);
+                }
+
+                for(int i = 0; i < jablka.size(); i++)
+                {
+                    if(X == jablka.get(i).getX() && Y == jablka.get(i).getY())
+                    {
+                        size++;
+                        jablka.remove(i);
+                        i++;
+                    }
+                }
+
+                for(int i =0; i < snake.size(); i++)
+                {
+                    if(X == snake.get(i).getX() && Y == snake.get(i).getY())
+                    {
+                        if(i != snake.size() - 1)
+                            stop();
+                    }
+                }
+
+                if(przechodzeniePrzezScany)
+                {
+                    if (X < 0)
+                        X = 39;
+                    if (X > 39)
+                        X = 0;
+                    if (Y < 0)
+                        Y = 39;
+                    if (Y > 39)
+                        Y = 0;
+                }
+                else
+                {
+                    if (X < 0 || X > 39 || Y < 0 || Y > 39)
                         stop();
                 }
-            }
 
-            if(x < 0 || x > 39 || y < 0 || y > 39)
-                stop();
-
-            z++;
-
-            if(z > szybkosc*100000)
-            {
                 if(prawo)
-                    x++;
-
+                    X++;
                 if(lewo)
-                    x--;
-
+                    X--;
                 if(gora)
-                    y--;
-
+                    Y--;
                 if(dol)
-                    y++;
+                    Y++;
 
-                z = 0;
-
-                wonsz = new Wonsz(x, y, 10);
+                wonsz = new Wonsz(X, Y, 10);
                 snake.add(wonsz);
 
                 if(snake.size() > size)
                     snake.remove(0);
             }
+            try {Thread.sleep(czekanie);} catch (Exception ignored) {}
             repaint();
         }
+        repaint();
     }
 
     @Override
@@ -193,6 +263,41 @@ class Okno extends JPanel implements Runnable, KeyListener
             prawo = false;
             dol = true;
         }
+        if (wcisniety == KeyEvent.VK_I)
+        {
+            if (info)
+                info = false;
+            else
+                info = true;
+        }
+
+        if(wcisniety == KeyEvent.VK_O && info)
+        {
+            if (przechodzeniePrzezScany)
+                przechodzeniePrzezScany = false;
+            else
+                przechodzeniePrzezScany = true;
+        }
+
+        if(wcisniety == KeyEvent.VK_ESCAPE)
+        {
+            if (pauza)
+                pauza = false;
+            else
+                pauza = true;
+        }
+
+        if(wcisniety == KeyEvent.VK_X && stopienSzybkosci > 0 && stopienSzybkosci <= 10)
+        {
+            czekanie += 10;
+            stopienSzybkosci--;
+        }
+
+        if(wcisniety == KeyEvent.VK_Z && stopienSzybkosci >= 0 && stopienSzybkosci < 10)
+        {
+            czekanie -= 10;
+            stopienSzybkosci++;
+        }
     }
     @Override
     public void keyReleased(KeyEvent arg0) {}
@@ -206,17 +311,23 @@ class Wonsz
     private int szerokosc;
     private int wysokosc;
 
-    public Wonsz(int x, int y, int tileSize)
+    public Wonsz(int x, int y, int rozmiar)
     {
         this.x = x;
         this.y = y;
-        this.szerokosc = tileSize;
-        this.wysokosc = tileSize;
+        this.szerokosc = rozmiar;
+        this.wysokosc = rozmiar;
     }
 
-    public void draw(Graphics g)
+    public void draw(Graphics g, int index)
     {
-        g.setColor(Color.BLACK);
+        if (index == 1600)
+            g.setColor(new Color(232, 232, 0));
+        else if (index % 2 == 0)
+            g.setColor(Color.BLACK);
+        else if (index % 2 != 0)
+            g.setColor(new Color(75, 75, 75));
+
         g.fillRect(x * szerokosc, y * wysokosc, szerokosc, wysokosc);
     }
     public int getX() {return x;}
@@ -236,12 +347,12 @@ class Jablko
     private int szerokosc;
     private int wysokosc;
 
-    public Jablko(int x, int y, int tileSize)
+    public Jablko(int x, int y, int rozmiar)
     {
         this.x = x;
         this.y = y;
-        this.szerokosc = tileSize;
-        this.wysokosc = tileSize;
+        this.szerokosc = rozmiar;
+        this.wysokosc = rozmiar;
     }
 
     public void draw(Graphics g)
