732a39fa144f418f6a5223fe21d0a14b2da40a2b
Konrad Zolynski <konrad.zolynski@gmail.com>
2020-04-23 18:03:53 +0200

    Main class edit

--- src/main/java/com/snake/apk/Main.java
+++ src/main/java/com/snake/apk/Main.java
@@ -1,7 +1,17 @@
 package com.snake.apk;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import javax.swing.*;
 
-import com.snake.implementation.Okno;
+import com.snake.implementation.PlayerDAO;
+import com.snake.gui.AdminFrame;
+import com.snake.gui.GameFrame;
+import com.snake.gui.LoginFrame;
+import com.snake.gui.RankingFrame;
+import com.snake.implementation.ConnectDatabase;
+import com.snake.implementation.StoredProcedures;
 
 /**
  * @author Konrad ¯o³yñski | https://github.com/Konrad-code | konrad.zolynski@gmail.com | +48 533 683 168
@@ -10,11 +20,33 @@ import com.snake.implementation.Okno;
  */
 
 public class Main
-{
+{	
     public static void main(String[] args)
     {
+    	StoredProcedures preparingProceduresInDatabase = StoredProcedures.getInstance();
+    	PlayerDAO player1 = new PlayerDAO();
+//    	player1.dropTable();
+//    	player1.createTable();
+//    	player1.deletePlayers();
+    	System.out.println(player1.getAllPlayers());
+    	System.out.println("\n\n");
+    	System.out.println(player1.searchPlayers("Brown"));
+//    	player1.deletePlayer("DavidDuhovny");
+    	
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+            	LoginFrame frame = new LoginFrame(true);
+                frame.setVisible(true);
+                frame.pack();
+                frame.setLocationRelativeTo(null);
+            }
+        });
+        
+        // OLD GAME EXECUTION
+        /*
         JFrame ramka = new JFrame();
-        Okno okno = new Okno();
+        GameFrame okno = new GameFrame();
 
         ramka.add(okno);
         ramka.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -23,5 +55,6 @@ public class Main
         ramka.pack();
         ramka.setLocationRelativeTo(null);
         ramka.setVisible(true);
+        */
     }
 }
